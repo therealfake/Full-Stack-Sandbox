eb2ca14b4f9c5f246716236e803069032e7fb960
Tianji Zhang <zhang.sh.tianji@gmail.com>
2022-07-11 20:25:25 -0400

    update to general function for gif search

--- JavaScript/APIs/gif-webpage-example/app.js
+++ JavaScript/APIs/gif-webpage-example/app.js
@@ -2,26 +2,20 @@ const img = document.querySelector('img');
 const search = document.querySelector('input');
 const btn = document.querySelector('button');
 
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=HcVZfgWKEepD3kqw5NRzXPKyAylHNV8C&s=cats', {mode: 'cors'})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        img.src = response.data.images.original.url;
-    });
-
-btn.addEventListener('click', () => {
-    url = 'https://api.giphy.com/v1/gifs/translate?api_key=HcVZfgWKEepD3kqw5NRzXPKyAylHNV8C&s=' + search.value;
-    fetch(url, {mode: 'cors'})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        img.src = response.data.images.original.url;
-    })
-    .catch(error => {
+async function getGIF(searchTerm){
+    try {
+        const url = 'https://api.giphy.com/v1/gifs/translate?api_key=HcVZfgWKEepD3kqw5NRzXPKyAylHNV8C&s=' + searchTerm;
+        const response = await fetch(url, {mode: 'cors'})
+        const imgData = await response.json()
+        img.src = imgData.data.images.original.url;
+    } catch (error) {
         errorMsg = document.createElement('div');
         errorMsg.textContent = error;
         document.appendChild(errorMsg);
-    })
+    }
+}
+
+getGIF("Hello")
+btn.addEventListener('click', () => {
+    getGIF(search.value)
 })
