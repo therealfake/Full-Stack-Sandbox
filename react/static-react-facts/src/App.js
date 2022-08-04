5d17c7a80a21739eea21d4b53ee756065960d367
Tianji Zhang <zhang.sh.tianji@gmail.com>
2022-08-03 23:42:46 -0400

    add react logo and switch to sass

--- react/static-react-facts/src/App.js
+++ react/static-react-facts/src/App.js
@@ -10,16 +10,6 @@ export default function App() {
     <div className="container">
       <Navbar />
       <Main />
-      <div>
-        {/* <h1>Fun facts about React</h1>
-        <ul>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 100K stars on GitHub</li>
-          <li>Is maintained by Facebook</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul> */}
-      </div>
     </div>
   );
 }
