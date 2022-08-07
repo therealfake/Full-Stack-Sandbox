d8ea57bcf1c347aaac3488fd187a3ff6307295d2
Tianji Zhang <zhang.sh.tianji@gmail.com>
2022-08-06 23:47:28 -0400

    complete top half of card

--- react/airbnb-experiences-page-clone/src/App.js
+++ react/airbnb-experiences-page-clone/src/App.js
@@ -1,12 +1,12 @@
 import './App.scss';
-import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero.js'
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <Header/>
+      <Navbar />
+      <Hero />
     </div>
   );
 }
-
-export default App;
