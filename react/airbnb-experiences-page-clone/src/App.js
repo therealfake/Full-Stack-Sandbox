45a694d40db330c1944a7f13589b6f7e5c3abf39
Tianji Zhang <zhang.sh.tianji@gmail.com>
2022-08-08 11:34:10 -0400

    use spread operator

--- react/airbnb-experiences-page-clone/src/App.js
+++ react/airbnb-experiences-page-clone/src/App.js
@@ -9,14 +9,7 @@ export default function App() {
     return ( 
       <Card 
         key={item.id}
-        item={item}
-        // img={`./images/${item.coverImg}`}
-        // rating={item.stats.rating}
-        // reviewCount={item.stats.reviewCount}
-        // location={item.location}
-        // title={item.title}
-        // price={item.price}
-        // openSpots={item.openSpots}
+        {...item}
       />
     )
   })
--- react/airbnb-experiences-page-clone/src/components/Card.js
+++ react/airbnb-experiences-page-clone/src/components/Card.js
@@ -1,25 +1,25 @@
 import React from "react";
 
 export default function Card(props) {
-    let badgeText
-    if (props.item.openSpots === 0) {
+    let badgeText;
+    if (props.openSpots === 0) {
         badgeText = "SOLD OUT";
-    } else if (props.item.location === "Online") {
+    } else if (props.location === "Online") {
         badgeText = "ONLINE"
     }
 
     return (
         <div className="card">
             {badgeText && <div className="badge">{badgeText}</div>}
-            <img src={`./images/${props.item.coverImg}`} className="card-image" />
+            <img src={`./images/${props.coverImg}`} className="card-image" />
             <div className="stats">
                 <img src="./images/star.png" alt="star icon" className="star" />
-                <span>{props.item.stats.rating}</span>
-                <span className="gray">({props.item.stats.reviewCount}) • </span>
-                <span className="gray">{props.item.location}</span>
+                <span>{props.stats.rating}</span>
+                <span className="gray">({props.stats.reviewCount}) • </span>
+                <span className="gray">{props.location}</span>
             </div>
-            <p>{props.item.title}</p>
-            <p><b>From ${props.item.price}</b></p>
+            <p>{props.title}</p>
+            <p><b>From ${props.price}</b></p>
         </div>
     )
 }
