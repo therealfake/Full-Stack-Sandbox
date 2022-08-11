cf29d99cec917af6e7fa0fbf5591820d3d7a8359
Tianji Zhang <zhang.sh.tianji@gmail.com>
2022-08-11 12:43:36 -0400

    use react state for image generation

--- react/meme-generator/src/components/Meme.js
+++ react/meme-generator/src/components/Meme.js
@@ -1,9 +1,18 @@
 import React from "react";
+import memes from "../memesData"
 
 export default function Meme(){
+    const [memeImage, setMemeImage] = React.useState("")
+    
+    function getMemeImage() {
+        const memesArray = memes.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        setMemeImage(memesArray[randomNumber].url);
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input 
                     type="text" 
                     className="form-input"
@@ -14,8 +23,14 @@ export default function Meme(){
                     className="form-input"
                     placeholder="Bottom text"
                 />
-                <button className="form-button">Get a new meme image 🖼</button>
-            </form>
+                <button 
+                    className="form-button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
+            <img src={memeImage} className="meme-image"/>
         </main>
     )
 }
