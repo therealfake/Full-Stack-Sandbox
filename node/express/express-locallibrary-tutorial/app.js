e64edf031271ba3e1890187e696093f1a7dc88db
Tianji Zhang <zhang.sh.tianji@gmail.com>
2024-07-03 16:49:30 -0400

    add routing

--- node/express/express-locallibrary-tutorial/app.js
+++ node/express/express-locallibrary-tutorial/app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog area of sites"
 
 const app = express();
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter); // Add catalog routes to the middleware chain
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
