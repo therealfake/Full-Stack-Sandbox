dcb339594f2e0638200932d8410e0e6d834c5606
Tianji Zhang <zhang.sh.tianji@gmail.com>
2024-07-06 10:25:07 -0400

    use luxon to make dates more friendly format

--- node/express/express-locallibrary-tutorial/models/bookinstance.js
+++ node/express/express-locallibrary-tutorial/models/bookinstance.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const { DateTime } = require("luxon");
 const Schema = mongoose.Schema;
 
 const BookInstanceSchema = new Schema({
@@ -19,6 +19,11 @@ BookInstanceSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/bookinstance/${this._id}`;
 });
+
+// Format date to more friendly format
+BookInstanceSchema.virtual("due_back_formatted").get( function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+})
 
 // Export model
 module.exports = mongoose.model("BookInstance", BookInstanceSchema);
